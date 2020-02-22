057b723bc838cd0db4a9fb4bc056b23587531618
shamsTheCoder <sktabrez856@gmail.com>
2020-02-22 14:43:55 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        var z = document.getElementById("btn");
+
+        function register() {
+            x.style.left = "-480px";
+            y.style.left = "10px";
+            z.style.left = "111px";
+        }
+        function login() {
+            x.style.left = "5px";
+            y.style.left = "450px";
+            z.style.left = "0px";
+        }
